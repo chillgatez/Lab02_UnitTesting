8819a7bef2b74fcd1be9ff44f2cd53ed4a944cda
Kelsee Watts <watts.kelsee@gmail.com>
2023-07-11 23:47:29 -0500

    implemented requierd methods.

--- Lab02_UnitTesting/Program.cs
+++ Lab02_UnitTesting/Program.cs
@@ -3,16 +3,26 @@ namespace Lab02_UnitTesting
 {
     internal class Program
     {
-        static public decimal Balance = 0;
-
         static void Main(string[] args)
         {
             UserInterface();
         }
+
+        static public decimal Balance = 0;
+
+
+
         static public decimal ViewBalance()
         {
             return Balance;
         }
-
+        static public decimal Withdraw(decimal amount)
+        {
+            return amount;
+        }
+        static public decimal Deposit(decimal amount)
+        {
+            return amount;
+        }
     }
 }
